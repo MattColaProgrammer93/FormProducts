55d36d0fae7728f5ffa3660522cda2cd356094ae
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-04-26 18:58:26 -0700

    Implemented VideoGame class and getVideoGame

--- ts/main.ts
+++ ts/main.ts
@@ -22,6 +22,30 @@ function addVideoGame(){
 
 function getVideoGame():VideoGame{
     // Create and returns video game
+    let game = new VideoGame();
+
+    // Get data from form
+    let titleInput = <HTMLInputElement>getById("title");
+    game.title = titleInput.value;
+
+    let priceInput = <HTMLInputElement>getById("price");
+    game.price = parseFloat(priceInput.value);
+
+    let ratingInput = <HTMLSelectElement>getById("rating");
+    game.rating = ratingInput.value
+
+    let digitalOnly = <HTMLInputElement>getById("online");
+    game.isDigitalOnly = digitalOnly.checked;
+
+    /*
+    if(digitalOnly.checked){
+        game.isDigitalOnly = true;
+    } else {
+        game.isDigitalOnly = false;
+    }
+    */
+
+    return game;
 }
 
 function displayGame(myGame:VideoGame):void{
@@ -41,3 +65,8 @@ myGame.title = "Mario";
 myGame.rating = "E";
 myGame.isDigitalOnly = true;
 */
+
+// Shortcut for getElementById
+function getById(id:string){
+    return document.getElementById(id);
+}
